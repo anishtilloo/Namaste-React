967ec3f245649039d083a3cbd0fe3cec0770c712
Anish Tilloo <anishtilloo@gmail.com>
2023-11-05 21:43:01 +0530

    episode 01 Inception

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// created an h1 element using React.createElement
+/* It takes 3 arguments 
+    - Tag Name as string
+        -- for example: 
+            'h1', 'div'
+    - Object of attributes
+        -- we can pass an empty object
+        -- or {id: "child"}, we are passing id as a child and many other attributes
+    - Content of the tag
+        -- we can either send a string or nest another element
+        -- 'hello world from react' or 
+            --- const h2 = React.createElement('h1', {}, 'hello world from react');
+                const h1 = React.createElement('h1', {}, h2);
+
+*/
+const h1 = React.createElement('h1', {}, 'hello world from react');
+       
+
+const root = document.getElementById('root');
+const reactRoot = ReactDOM.createRoot(root);
+reactRoot.render(h1);
